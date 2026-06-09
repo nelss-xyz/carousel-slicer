1b5621290c051b7a4c73df0cad3416a5312ad453
Nelson <nelso.n@aol.com>
2026-06-09 11:29:15 +0800

    bugfix 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/carousel-slicer/',
+  base: '/carousel-slicer/dist',
   plugins: [react()],
 })
